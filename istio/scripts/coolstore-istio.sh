bbb4c5b46f1bc2e9e660a90161692ab2ef5dada3
Yuri Pismerov <ypismerov@gmail.com>
2021-10-07 18:12:20 -0400

    update

--- istio/scripts/coolstore-istio.sh
+++ istio/scripts/coolstore-istio.sh
@@ -9,4 +9,4 @@ spec:
     - user6-inventory-dev
 EOF
 oc apply -n user6-coolstore-dev -f $CHE_PROJECTS_ROOT/cloud-native-workshop-v2m1-labs/istio/inventory-gateway.yml
-oc patch -n user6-inventory-dev virtualservice/inventory --type='json' -p '[{"op":"add","path":"/spec/hosts","value": ["istio-ingressgateway-user6-istio-system.apps.cluster-4k8mv.4k8mv.sandbox1663.opentlc.com"]}]'
+oc patch -n user6-coolstore-dev virtualservice/inventory --type='json' -p '[{"op":"add","path":"/spec/hosts","value": ["istio-ingressgateway-user6-istio-system.apps.cluster-4k8mv.4k8mv.sandbox1663.opentlc.com"]}]'
