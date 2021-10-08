f35e614c2dd5ed684176bc203d21cac475012ee6
user6 <user6@no-reply.com>
2021-10-08 14:57:50 +0000

    updated

--- istio/scripts/coolstore-istio.sh
+++ istio/scripts/coolstore-istio.sh
@@ -9,7 +9,7 @@ spec:
   members:
     - user6-coolstore-dev
 EOF
-oc patch -n user6-coolstore-dev coolstore --patch '{"spec":{"template":{"metadata":{"annotations":{"sidecar.istio.io/inject:"true"}}}}' --type=merge
+oc patch dc coolstore -n user6-coolstore-dev --patch '{"spec":{"template":{"metadata":{"annotations":{"sidecar.istio.io/inject":"true"}}}}' --type=merge
 oc apply -n user6-coolstore-dev -f $CHE_PROJECTS_ROOT/cloud-native-workshop-v2m1-labs/istio/coolstore-inventory-gateway.yml
 oc patch -n user6-coolstore-dev virtualservice/inventory --type='json' -p '[{"op":"add","path":"/spec/hosts","value": ["istio-ingressgateway-user6-istio-system.apps.cluster-4k8mv.4k8mv.sandbox1663.opentlc.com"]}]'
 oc apply -n user6-coolstore-dev -f $CHE_PROJECTS_ROOT/cloud-native-workshop-v2m1-labs/istio/coolstore-destination-rule-all.yaml
---
+++ istio/scripts/inventory-istio.sh
@@ -0,0 +1,15 @@
+git pull
+oc apply -f - <<EOF
+apiVersion: maistra.io/v1
+kind: ServiceMeshMemberRoll
+metadata:
+  name: default
+  namespace: user9-istio-system 
+spec:
+  members:
+    - user9-coolstore-dev
+EOF
+oc patch dc inventory -n user9-coolstore-dev --patch '{"spec":{"template":{"metadata":{"annotations":{"sidecar.istio.io/inject":"true"}}}}' --type=merge
+oc apply -n user9-coolstore-dev -f $CHE_PROJECTS_ROOT/cloud-native-workshop-v2m1-labs/istio/inventory-inventory-gateway.yml
+oc patch -n user9-coolstore-dev virtualservice/inventory --type='json' -p '[{"op":"add","path":"/spec/hosts","value": ["istio-ingressgateway-user9-istio-system.apps.cluster-4k8mv.4k8mv.sandbox1663.opentlc.com"]}]'
+oc apply -n user9-coolstore-dev -f $CHE_PROJECTS_ROOT/cloud-native-workshop-v2m1-labs/istio/inventory-destination-rule-all.yaml
